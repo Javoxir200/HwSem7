c85e906e472f0cfc07fad09972566efccb11f286
MJ <javohirbek6189@gmail.com>
2024-07-22 22:01:12 +0300

    hw Comp

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 1: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+*/
+
+
+// функция натуральные числа от M до N
+class Program
+{
+    static void PrintNumbers(int m, int n)
+    {
+        if (m > n) return;
+        Console.WriteLine(m);
+        PrintNumbers(m + 1, n);
+    }
+
+    static void Main(string[] args)
+    {
+        int m = 5; // set M
+        int n = 15; // set N
+        PrintNumbers(m, n);
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+*/
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (m > 0 && n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = Ackermann(m, n);
+        Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+    }
+}
